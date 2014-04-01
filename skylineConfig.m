0f2ff7d5bfb516fc9e769693d3d6afb85b1e2d19
Subhransu Maji <subhransu.maji@gmail.com>
2014-04-01 18:50:06 -0500

    rename annotations -> labels

--- skylineConfig.m
+++ skylineConfig.m
@@ -4,7 +4,8 @@ conf.path.dataset = '../data';  % Set this to the path where you have downloaded
 conf.path.cache = '../cache'; % Set this to where to cache intermediate results
 conf.path.anno = fullfile(conf.path.cache, 'anno.mat');
 conf.path.image = fullfile(conf.path.dataset, 'images');
-conf.path.annotations = fullfile(conf.path.dataset, 'annotations');
+conf.path.labels = fullfile(conf.path.dataset, 'labels');
+conf.path.regions = fullfile(conf.path.dataset, 'regions');
 conf.path.seeds = fullfile(conf.path.dataset, 'seeds');
 conf.path.imageSet = fullfile(conf.path.dataset, 'imageSets');
 
