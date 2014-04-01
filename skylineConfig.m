ff03af0df6be422dcdecc314d1d74e62b1c44a94
Subhransu Maji <subhransu.maji@gmail.com>
2014-04-01 13:07:12 -0500

    typo in imageSet path

--- skylineConfig.m
+++ skylineConfig.m
@@ -5,5 +5,5 @@ conf.path.cache = '../cache'; % Set this to where to cache intermediate results
 conf.path.anno = fullfile(conf.path.cache, 'anno.mat');
 conf.path.image = fullfile(conf.path.dataset, 'images');
 conf.path.annotations = fullfile(conf.path.dataset, 'annotations');
-conf.path.imageSet = fullfile(conf.path.dataset, 'imageSet');
+conf.path.imageSet = fullfile(conf.path.dataset, 'imageSets');
 
