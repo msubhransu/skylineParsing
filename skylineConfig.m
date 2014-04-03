1ca45e3caefb79b69add9b01f6b31dd1a65833c1
Subhransu Maji <subhransu.maji@gmail.com>
2014-04-03 07:38:23 -0500

    more params + parsing code

--- skylineConfig.m
+++ skylineConfig.m
@@ -8,8 +8,19 @@ conf.path.labels = fullfile(conf.path.dataset, 'labels');
 conf.path.regions = fullfile(conf.path.dataset, 'regions');
 conf.path.seeds = fullfile(conf.path.dataset, 'seeds');
 conf.path.imageSet = fullfile(conf.path.dataset, 'imageSets');
-
+conf.path.texton = fullfile(conf.path.dataset, 'textons');
 
 % SLIC segmentation params
-conf.slic.regionSize = 30;
-conf.slic.regularizer = 0.001;
+conf.param.slic.regionSize = 30;
+conf.param.slic.regularizer = 0.001;
+
+% Texton library parameters
+conf.param.texton.no = 6;      %
+conf.param.texton.ss = 1;      %
+conf.param.texton.ns = 2;      %
+conf.param.texton.sc = sqrt(2);%
+conf.param.texton.el = 2;      %
+conf.param.texton.k  = 32;     % number of textons in the library
+conf.param.texton.radius = 20; % radius of region for histogram computation
+
+
--- skylineParse.m
+++ skylineParse.m
@@ -6,13 +6,54 @@ data.image.gray = rgb2gray(data.im)/255;
 data.image.rgb  = im2single(data.im);
 
 % Compute super-pixels using SLIC
-data.segments = vl_slic(data.image.lab, conf.slic.regionSize, conf.slic.regularizer);
+data.segments = vl_slic(data.image.lab, conf.param.slic.regionSize, conf.param.slic.regularizer);
+
+% Assign segments to the labels (initial segmentation)
+segLabel = labelSegments(data);
 
 % Compute data terms
 data.score = localScore(conf, data);
 
 % Label the regions
 label = [];
+
+%--------------------------------------------------------------------------
+%                                                        Perform initial
+%                                                        labelling
+%--------------------------------------------------------------------------
+function label = labelSegments(data)
+% Assign each seed its majority label
+% 1 backgroud, 2 unknown, 3:N+2 buildings
+numSeeds = length(data.seeds);
+numSegments = max(data.segments(:));
+seed2segment = zeros(numSeeds, numSegments);
+label = zeros(size(data.labels), 'uint32');
+
+% Create a seed map
+for i = 1:length(data.seeds), 
+    fg = data.seeds{i}; %x, y format
+    pixelInd = sub2ind(size(label), fg(:,2), fg(:,1));
+    segmentId = data.segments(pixelInd);
+    for j = 1:length(segmentId), 
+        seed2segment(i,segmentId(j)) =  seed2segment(i,segmentId(j)) + 1;
+    end
+end
+
+% Assign labels to segments
+for i = 1:numSegments,
+    [maxVal, maxInd] = max(seed2segment(:,i));
+    if maxVal == 0
+        label(data.segments == i) = 2;
+    else
+        label(data.segments == i) = maxInd + 1;
+    end
+end
+
+% All pixels above or below the middle region are background
+for i = 1:size(label,2)
+    label(1:data.region(i,1),i) = 1;
+    label(data.region(i,2):end,i) = 1;
+end
 
 %--------------------------------------------------------------------------
 %                                                        Compute data terms
@@ -29,8 +70,10 @@ score.combined = alpha*(beta*score.color + (1-beta)*score.texture) + (1-alpha)*s
 
 
 
+%--------------------------------------------------------------------------
+%                                                        Compute data terms
+%--------------------------------------------------------------------------
 function score = textureScore(conf, data)
-
 % Load params of the texton filters (from BSDS segmentation)
 no = conf.param.texton.no;
 ss = conf.param.texton.ss;
@@ -38,6 +81,7 @@ ns = conf.param.texton.ns;
 sc = conf.param.texton.sc;
 el = conf.param.texton.el;
 k  = conf.param.texton.k ;
+radius = conf.param.texton.radius;
 
 % Load textons (these are precomputed)
 fileName = sprintf('unitex_%.2g_%.2g_%.2g_%.2g_%.2g_%d.mat',no, ss, ns, sc, el, k);
@@ -45,4 +89,6 @@ load(fullfile(conf.path.texton, fileName), 'fb', 'tex', 'tsim');
 
 % Assign textons to pixels in the image
 texMap = assignTextons(fbRun(fb, data.image.gray), tex);
-texHist = getTexture(texMap, 32, 20); % Figure out what 20 is 
+
+% Compute local histograms at each pixel by pooling textons within a radius
+texHist = getTexture(texMap, k, radius);
