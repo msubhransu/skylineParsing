8ef911d8dff1f0896973e8019d9d5c39498d183a
Subhransu Maji <subhransu.maji@gmail.com>
2014-04-12 18:25:17 -0500

    no silly green lines

--- parse/showParse.m
+++ parse/showParse.m
@@ -1,10 +1,7 @@
 function showParse(im, parse)
 imagesc(im); axis image off; hold on;
-
 [~,w,~] = size(im);
-
-plot(1:w, parse.tiers(parse.order(end),:),'g-','LineWidth',2);
-for i = length(parse.order)-1:-1:1 
+for i = length(parse.order):-1:1
     plot(1:w, parse.tiers(parse.order(i),:),'r-','LineWidth',2);
 end
 plot(1:w, parse.lower,'r-','LineWidth',2);
