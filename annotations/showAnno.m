a4cf26f92ef156c659cf65fde8a41b9e7ad2b821
Subhransu Maji <subhransu.maji@gmail.com>
2014-04-29 11:10:57 -0500

    code to showAnno

--- annotations/showAnno.m
+++ annotations/showAnno.m
@@ -1,11 +1,8 @@
 function showAnno(anno, imageSet)
-% SHOWANNO shows all the annotations for a particular city
-%   SHOWANNO(CITY) interactively loads and displays all the images and
-%   annotations for the CITY. Press arrow keys to move from one image to
-%   another. Pressing SPACE toggles the overlay of the boundaries on the
-%   image.
-%
-% Author: Subhransu Maji
+% SHOWANNO interactively displays the annotations
+%   SHOWANNO(ANNO, IMAGESET) interactively shows all the annotations in
+%   ANNO that belong to images in IMAGESET. Press 'h' for help and 'esc' 
+%   to exit the display.
 
 conf = skylineConfig();
 
@@ -14,15 +11,16 @@ if nargin > 1,
     anno = selectAnno(anno, anno.object.imageSet == setId);
 end
 
+% Global variables to keep state
 global g;
 g.conf = conf;
 g.anno = anno;
 g.curId = 1;
-g.edgeOverlay = false;
 g.showSeeds = true;
 g.se = strel('diamond', 3);
 numImages = length(anno.object.image);
 
+% Loop over images and display
 while(1),
     drawAnno();
     while 1,
@@ -31,8 +29,7 @@ while(1),
             break;
         end
     end
-    
-    % Loop and display
+    % Change state depending on input
     switch b
         case 29
             if g.curId < numImages
@@ -44,8 +41,6 @@ while(1),
             end
         case 27
             break;
-        case 'e'
-            g.edgeOverlay = ~g.edgeOverlay;
         case 's'
             g.showSeeds = ~g.showSeeds;
         case 'g' %jump to a specified image index
@@ -56,52 +51,61 @@ while(1),
                     g.curId = answer;
                 end
             end
+        case 'h'
+            displayHelp();
     end
 end
+
+%--------------------------------------------------------------------------
+%                                            display help message for usage
+%--------------------------------------------------------------------------
+function displayHelp()
+fprintf('Shortcut key bindings for display\n');
+fprintf('    h: print this help message\n');
+fprintf('  esc: exit display\n');
+fprintf(' Navigation:\n');
+fprintf('   ->: next image\n');
+fprintf('   <-: prev image\n');
+fprintf('    g: jump to image\n');
+fprintf(' Display:\n');
+fprintf('    s: toggle seeds\n');
+    
 %--------------------------------------------------------------------------
 %                                        draw the annotations for the image
 %--------------------------------------------------------------------------
 function drawAnno()
 global g;
-
 i = g.curId;
-
 data = getData(g.conf, g.anno, i);
-%data = resizeData(data, 0.3);
 
-% Add a red boundary on the buildings
-if g.edgeOverlay,
-    ee = (data.labels == 0);
-    ee = imdilate(ee,g.se); 
-    im(ee > 0) = 255;
-end
-
-% Plot the image and the boundaries
+% Plot the image
 figure(1); clf;
 vl_tightsubplot(1,2,1);
 imagesc(data.im); axis image off; hold on;
 
 % Plot upper and lower tiers
-plot(1:size(data.region,1), data.region(:,1),'w-','LineWidth', 4);
-plot(1:size(data.region,1), data.region(:,1),'b--','LineWidth',4);
+plot(1:size(data.region,1), data.region(:,1),'w-','LineWidth', 2);
+plot(1:size(data.region,1), data.region(:,1),'b--','LineWidth',2);
 
-plot(1:size(data.region,1), data.region(:,2),'w-','LineWidth', 4);
-plot(1:size(data.region,1), data.region(:,2),'b--','LineWidth',4);
+plot(1:size(data.region,1), data.region(:,2),'w-','LineWidth', 2);
+plot(1:size(data.region,1), data.region(:,2),'b--','LineWidth',2);
 
-% Display the seeds if enabled
+% Display the seeds
 if g.showSeeds, 
     for s = 1:length(data.seeds), 
         plot(data.seeds{s}(:,1), data.seeds{s}(:,2), 'y.');
     end
 end
+
+% Display info
 handle=title(sprintf('City: %s (%i/%i)  Image: %i/%i ImageSet:%i', data.city, ...
                      g.anno.object.cityId(i), max(g.anno.object.cityId), ...
                      i, length(g.anno.object.image), ...
                      g.anno.object.imageSet(i)));
 set(handle, 'Interpreter','none');
 
-% Plot the segmentation labels
-vl_tightsubplot(1,2,2);
+% Display the segmentation labels
+vl_tightsubplot(1,2,2); % from vlfeat
 imagesc(data.labels); axis image off;
 title(sprintf('Ground truth labels (%i x %i px)', size(data.labels,2), size(data.labels,1)));
 colormap(hot);
