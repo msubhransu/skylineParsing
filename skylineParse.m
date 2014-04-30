0c98e556d8e5ec8df19f67b766e9480dc802c064
Subhransu Maji <subhransu.maji@gmail.com>
2014-04-30 13:02:38 -0500

    docs

--- skylineParse.m
+++ skylineParse.m
@@ -1,4 +1,17 @@
 function parse = skylineParse(conf, data, method)
+% SKYLINEPARSE parses the buildings in a skyline image
+%   PARSE = SKYLINEPARSE(CONF, DATA, METHOD) parses the image contained in
+%   DATA using the seeds and tiers provided. The METHOD is one of
+%   {rectangle, refined, tiered, or standard} specifying the various MRF
+%   formulations. If the unary terms are not provided they are computed as
+%   a part of the routing.
+%
+% Output
+%   PARSE is a structure containing each building parsed
+%
+% Author: Subhransu Maji
+
+
 % Compute unary potentials if they are not provided
 if ~isfield(data,'unary')
     disp('Preparing data..');
