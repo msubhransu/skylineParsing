5fec5801a116893344e148745608053fbcaf8cd0
Subhransu Maji <subhransu.maji@gmail.com>
2014-05-04 19:23:57 -0500

    compile instructions

--- compile.m
+++ compile.m
@@ -9,18 +9,29 @@ if ~exist('bin', 'file')
     mkdir('bin');
 end
 
+% Pass additional flags to the cxx compiler
+% Set this flag on OSX 10.9 and clang compiler.
+% CXXFLAGS='-std=c++11'; 
+
+% On linux machines set this flag to empty
+CXXFLAGS=''; 
+
 % Mex code for rectangle parsing
-mex -O CXXFLAGS="-std=c++11" parse/mexOptRectangle.cc -outdir bin
+cmd=sprintf('mex -O CXXFLAGS="%s" parse/mexOptRectangle.cc -outdir bin', CXXFLAGS);
+eval(cmd);
 disp('done compiling..mexOptRectangle.');
 
 % Mex code for tiered parsing
-mex -O CXXFLAGS="-std=c++11" parse/mexOptTiered.cc -outdir bin
+cmf=sprintf('mex -O CXXFLAGS="%s" parse/mexOptTiered.cc -outdir bin', CXXFLAGS);
+eval(cmd);
 disp('done compiling..mexOptTiered.');
 
 % Mex code for maxflow
-mex -O -largeArrayDims CXXFLAGS="-std=c++11" extern/maxflow/maxflowmex.cpp extern/maxflow/graph.cpp extern/maxflow/maxflow.cpp  -outdir bin
+cmd=sprintf('mex -O -largeArrayDims CXXFLAGS="%s" extern/maxflow/maxflowmex.cpp extern/maxflow/graph.cpp extern/maxflow/maxflow.cpp  -outdir bin', CXXFLAGS);
+eval(cmd);
 disp('done compiling..maxflow.');
 
 % Mex code for computing weight matrix
-mex -O CXXFLAGS="-std=c++11" extern/maxflow/mexComputeWeightMatrix.cc  -outdir bin
+cmd=sprintf('mex -O CXXFLAGS="%s" extern/maxflow/mexComputeWeightMatrix.cc  -outdir bin', CXXFLAGS);
+eval(cmd);
 disp('done compiling..mexComputeWeightMatrix.');
